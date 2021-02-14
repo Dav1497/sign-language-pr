0734b5d199643b85d90ef299744fcdc10f244b22
estefaniatc <estefania.torres1@upr.edu>
2021-02-14 13:31:43 -0400

    model to js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ async function load_model() {
   // It's possible to load the model locally or from a repo
   // You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
   //const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
-  const model = await loadGraphModel("https://raw.githubusercontent.com/Dav1497/sign-language-pr/tensorflowjs/backend/venv/web_model/model.json");
+  const model = await loadGraphModel("https://raw.githubusercontent.com/Dav1497/sign-language-pr/tensorflowjs/web_model/model.json");
   return model;
 }
 

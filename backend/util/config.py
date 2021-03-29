b51f8f921a43b20e24009e5894a7b6c29f67106b
estefaniatc <estefania.torres1@upr.edu>
2021-03-29 09:15:40 -0400

    frontend fixes

--- backend/util/config.py
+++ backend/util/config.py
@@ -6,7 +6,7 @@ DEV_DB = 'postgresql://aslesusr:1234@localhost/SignLanguagePR'
 PROD_DB = 'postgres://oznwwrrmbzaouj:c3734637920c26912d7cebad22d33eb971c8925425c4f2bf342d326bd1d4cb1a@ec2-3-211-37-117.compute-1.amazonaws.com:5432/d2cm180srol83l'
 
 app = flask.Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = PROD_DB
+app.config['SQLALCHEMY_DATABASE_URI'] = DEV_DB
 app.secret_key = "asles"
 
 db = SQLAlchemy(app)
--- src/HomePage.js
+++ src/HomePage.js
@@ -4,6 +4,7 @@ import Nav from './Nav';
 import axios from "axios";
 import { SERVER_URL, headers } from "./Signup";
 import { Redirect, withRouter } from "react-router-dom";
+import "./HomePage.css";
 
 class HomePage extends React.Component {
 
@@ -51,40 +52,42 @@ class HomePage extends React.Component {
                         loggedInUserId={this.state.loggedInUser.user_id}
                         loggedInUserName={this.state.loggedInUser.name} />}
                 </div>
-                <table style={{marginLeft:"2.5%"}}>
-                    <tr>
-                        <td style={{ verticalAlign: "top"}}>
-                            <div>
-                            <hr></hr>
-                                <p style={{ color: "#438CFB", fontFamily: "Varela Round", fontSize: "20px" }}>                                    
-                                    DATOS CURIOSOS
+                <table className="mytable">
+                    <tbody>
+                        <tr>
+                            <td className="leftCol">
+                                <div>
+                                    <hr></hr>
+                                    <p style={{ color: "#438CFB", fontFamily: "Varela Round", fontSize: "20px" }}>
+                                        DATOS CURIOSOS
                                 </p>
-                                <p style={{ color: "white", fontFamily: "Varela Round", fontSize: "20px" }}>
-                                    El Instituto de Estadísticas del Gobierno de Puerto Rico reportó que para el año 2018, la población de adultos sordos o con problemas auditivos
-                                    severos fue estimada en 218,495 personas.
-                                    Según el censo, nuestro país contó con 2,601,142 habitantes en ese año,
+                                    <p style={{ color: "white", fontFamily: "Varela Round", fontSize: "20px" }}>
+                                        El Instituto de Estadísticas del Gobierno de Puerto Rico reportó que para el año 2018, la población de adultos sordos o con problemas auditivos
+                                        severos fue estimada en 218,495 personas.
+                                        Según el censo, nuestro país contó con 2,601,142 habitantes en ese año,
                                     lo cual significa que <bold>8.4%</bold> de la población de Puerto Rico se constituye de personas sordas.
                                     <hr></hr>
                                     Las agencias gubernamentales estatales no están preparadas para atender a personas de la comunidad sorda.
                                     <hr></hr>
-                                </p><br/>
-                            </div>
-                            <div style={{textAlign:'left'}}>
-                                {/* https://gurabo.uagm.edu/sites/default/files/uploads/Health-Sciences/Thesis/2018/Elisamuel-Rivera-PHL-2018.pdf */}
-                                <img style={{width:'100px'}} src='https://drive.google.com/uc?export=view&id=1S3BoN6iaIcI6U-IHHwoFQ9Yg6MdHtCO9' alt=""/>
-                            </div>
-
-                        </td>
-                        <td style={{ width: '50%' }}>
-                            <br></br>
-                            {this.state.levels && this.state.levels.length > 0 && this.state.levels.map(level => (
-                                <div>
-                                    <LevelBox levelName={level.level_name} levelId={level.level_id}></LevelBox>
-                                    <div style={{ height: "50px" }}><br /></div>
+                                    </p><br />
                                 </div>
-                            ))}
-                        </td>
-                    </tr>
+                                <div style={{ textAlign: 'left' }}>
+                                    {/* https://gurabo.uagm.edu/sites/default/files/uploads/Health-Sciences/Thesis/2018/Elisamuel-Rivera-PHL-2018.pdf */}
+                                    <img style={{ width: '100px' }} src='https://drive.google.com/uc?export=view&id=1S3BoN6iaIcI6U-IHHwoFQ9Yg6MdHtCO9' alt="" />
+                                </div>
+
+                            </td>
+                            <td>
+                                <br></br>
+                                {this.state.levels && this.state.levels.length > 0 && this.state.levels.map(level => (
+                                    <div>
+                                        <LevelBox levelName={level.level_name} levelId={level.level_id}></LevelBox>
+                                        <div style={{ height: "50px" }}><br /></div>
+                                    </div>
+                                ))}
+                            </td>
+                        </tr>
+                    </tbody>
                 </table>
             </body>);
     }
